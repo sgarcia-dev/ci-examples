62706fe67dc1f962b39e6d0728f65a5cfe13a3b7
Sergei Garcia <sergei.garcia@ipointsystems.com>
2016-07-08 16:27:41 -0500

    pushed current CI repositories

---
+++ webpack-angular/app/index.js
@@ -0,0 +1,3 @@
+import angular from 'angular';
+
+const ngModule = angular.module('app', []);
---
+++ webpack-react-sass/webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack'),
+	path = require('path');
+
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/assets/',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		inline: true,
+		port: 3333
+	},
+	module: {
+		loaders: [
+			{test: /\.css$/, loader: "style!css"},
+			{test: /\.scss$/, loader: "style!css!sass"},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	},
+	plugins: [
+		new webpack.OldWatchingPlugin()
+	]
+};
